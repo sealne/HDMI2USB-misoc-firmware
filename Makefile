timvideos/litex-buildenv

commit 4aec1c23c0c58f936aea47373ddc6beec149454c

Makefile: for simplify for optis

Makefile
@@ -1,13 +1,13 @@
-BOARD ?= atlys
+BOARD ?= opsis
 MSCDIR ?= ../misoc
 PROG ?= impact
 SERIAL ?= /dev/ttyVIZ0
-TARGET ?= hdmi2usb
+TARGET ?= base
 
-HDMI2USBDIR = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+HDMI2USBDIR = ../HDMI2USB-misoc-firmware
 PYTHON = python3
 
-CMD = $(PYTHON) make.py -X $(HDMI2USBDIR) -t $(BOARD)_$(TARGET) -Ot firmware_filename $(HDMI2USBDIR)/firmware/lm32/firmware.bin -Op programmer $(PROG)
+CMD = $(PYTHON) make.py -X $(HDMI2USBDIR) -t $(BOARD)_$(TARGET) -Op programmer $(PROG)
 
 ifeq ($(OS),Windows_NT)
 	FLTERM = $(PYTHON) $(MSCDIR)/tools/flterm.py
@@ -30,7 +30,7 @@ help:
 	@echo "   PROG=programmer      (current: $(PROG))"
 	@echo " SERIAL=serial port     (current: $(SERIAL))"
 
-gateware: lm32-firmware
+gateware:
 	cd $(MSCDIR) && $(CMD) --csr_csv $(HDMI2USBDIR)/test/csr.csv clean
 	cp hdl/encoder/vhdl/header.hex $(MSCDIR)/build/header.hex
 	cd $(MSCDIR) && $(CMD) --csr_csv $(HDMI2USBDIR)/test/csr.csv build-csr-csv build-bitstream

sealne/HDMI2USB-misoc-firmware

commit e300def712d6532fbb35edbf1ac7d9f92214144f

Change Makefile to autodetect path again.
(Was broken in 4aec1c23c0c58f936aea47373ddc6beec149454c)

Makefile
@@ -4,7 +4,7 @@ PROG ?= impact
 SERIAL ?= /dev/ttyVIZ0
 TARGET ?= hdmi2usb
 
-HDMI2USBDIR = ../HDMI2USB-misoc-firmware
+HDMI2USBDIR = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 PYTHON = python3
 
 CMD = $(PYTHON) make.py -X $(HDMI2USBDIR) -t $(BOARD)_$(TARGET) -Ot firmware_filename $(HDMI2USBDIR)/firmware/lm32/firmware.bin -Op programmer $(PROG)