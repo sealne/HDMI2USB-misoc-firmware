c15dd68eb8e322114df4654f1d4ef88b028fca5e
Kenny Duffus <kenny@duffus.org>
2015-10-03 19:29:09 +0100

    Strip out the diff header lines

--- .travis/run.sh
+++ .travis/run.sh
@@ -110,7 +110,7 @@ for BOARD in $BOARDS; do
 		else
 			echo "make clean left these files behind:"
 			echo ""
-			diff -u /tmp/filelist.before /tmp/filelist.after|grep -v "@@"|grep "+"
+			diff -u /tmp/filelist.before /tmp/filelist.after|grep -v "@@"|grep -v "+++"|grep -v "---"|grep "+"
 			echo "============================================="
 			exit
 		fi
